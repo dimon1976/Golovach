08d2446d55d14ebf738261844dec9271762dc580
Dzmitry <dima2000@tut.by>
2021-10-17 19:30:47 +0300

    Сделал запись буферризированного потока в файл

---
+++ src/Stream/Main.java
@@ -0,0 +1,21 @@
+package Stream;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner r = new Scanner(System.in);
+        String test = r.nextLine();
+
+        try (FileOutputStream fos = new FileOutputStream("test.txt");   // указываем имя файла для записи
+             BufferedOutputStream bos = new BufferedOutputStream(fos)) {  // создаем буферизированный поток и берем имя файла куда писать будем
+
+            byte[]array = test.getBytes();  // обязательно байтовый массив
+            bos.write(array,0, array.length); // пишем в файл
+
+        } catch (IOException e) {  // ловим возможную ошибку
+            System.out.println(e.getStackTrace());
+        }
+    }
+}
